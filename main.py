dea5c688138b6418779eee906add30e9919ec556
unknown <aurorapariseau@gmail.com>
2022-07-14 13:31:59 -0500

    Added IP passing and verification. Added grid. Condensed to single figure. Added text to drops.

--- main.py
+++ main.py
@@ -1,55 +1,81 @@
 from ping3 import ping, verbose_ping
 from matplotlib import pyplot as plt
-from time import sleep, time
+from time import sleep, time, ctime
+from itertools import cycle
+from ipaddress import ip_address
+import sys
 
+graph_delay = .05
+cycol = cycle('bgrcmk')
 
 if __name__ == '__main__':
-    default_gateway = '192.168.0.1'
-    destination_test = '8.8.8.8'
+    test_ips = []
 
-    results = {
-        f'{default_gateway}': {'lost': 0, 'return time': [], 'graph': None},
-        f'{destination_test}': {'lost': 0, 'return time': [], 'graph': None},
-    }
-    delay = .1
+    argc = len(sys.argv) - 1
+    if not argc:
+        test_ips.append('192.168.0.1')
+        test_ips.append('8.8.8.8')
+    else:
+        for arg in sys.argv[1:]:
+            test_arg = arg.strip().replace('"', '').replace("'", "")
+            test_arg = ip_address(test_arg)
+            test_ips.append(test_arg)
 
-    figure, axis = plt.subplots(1, len(results.keys()))
-    plt.ion()
-    plt.show()
+    if not test_ips:
+        raise RuntimeError("No valid IP address passed")
 
-    count = 0
-    for ip in results.keys():
-        results[ip]['graph'] = count
-        count += 1
+    # Default values in times allow for average to be taken to plot dropped packets nicely
+    results = {}
+    for ip in test_ips:
+        results[ip] = {'return time': [.1], 'graph_color': next(cycol), 'times': [int(time())], 'lost': []}
 
     start = int(time())
 
-    for x in range(10, 99999999999999):
+    fig = plt.figure()
+    fig.canvas.manager.set_window_title("Internet Drop Tracker")
+    plt.ion()
+    plt.show()
+    while True:
+        plt.clf()
 
+        plt.title("Ping return times (in seconds)")
+        plt.ylabel("Return Time (s)")
+        plt.xlabel("Time of Event")
         for ip in results.keys():
 
-            result = ping(ip)
+            result = ping(format(ip))
             if not result:
-                results[ip]['lost'] += 1
+                results[ip]['lost'].append(int(time()))
             else:
                 last_x_increment = 10
 
+                results[ip]['times'].append(int(time()))
                 results[ip]['return time'].append(result)
 
-                axis[results[ip]['graph']].clear()
-                axis[results[ip]['graph']].plot(list(range(0, len(results[ip]['return time']))),
-                                                results[ip]['return time'],
-                                                label=f"Pinging {ip}")
-                axis[results[ip]['graph']].set_title(f"Return Time Over {int(time()) - start}s\n"
-                                                     f"Packet Loss: { results[ip]['lost'] / (results[ip]['lost'] + len(results[ip]['return time'])) * 100:.2f}%\n"
-                                                     f"Avg Return Time: {sum(results[ip]['return time'])/len(results[ip]['return time']):.4f}s\n"
-                                                     f"Avg Over Last {last_x_increment}: {sum(results[ip]['return time'][-last_x_increment:])/last_x_increment:.4f}s\n"
-                                                     f"Max Time: {max(results[ip]['return time']):.4f}s  Min Time: {min(results[ip]['return time']):.4f}s")
-                axis[results[ip]['graph']].set_xlabel(f"Trials")
-                axis[results[ip]['graph']].legend()
+            x_legend = list(map(ctime, results[ip]['times'][1:]))
+            plt.plot(x_legend, results[ip]['return time'][1:], color=results[ip]['graph_color'], alpha=.5,
+                     label=f"Pinging {ip}")
+
+            if results[ip]['lost']:
+                x_legend = list(map(ctime, results[ip]['lost']))
+                y_legend = [(max(results[ip]['return time'][1:]) + min(results[ip]['return time'][1:])) / 2] * \
+                           len(results[ip]['lost'])
+                plt.scatter(x_legend, y_legend, marker='o', label=f"Drops to {ip}", c=results[ip]['graph_color'])
+                for x,y in zip(x_legend, y_legend):
+                    plt.text(x, y, 'Drop', horizontalalignment='right')
+
+        #plt.grid(xdata=results[ip]['times'][1:], ydata=results[ip]['return time'][1:])
+        plt.minorticks_on()
+        plt.grid(which='minor')
+        plt.grid(which='major')
+        times = results[list(results.keys())[0]]['times'][1:]
+        x_times = [times[0], times[int(len(times) * .25)], times[int(len(times) * .5)], times[int(len(times) * .75)],
+                   times[-1]]
+        x_times = list(map(ctime, x_times))
+        plt.xticks(x_times, rotation=15)
+        plt.legend(loc='upper left')
         plt.draw()
         plt.gcf().canvas.draw_idle()
         plt.gcf().canvas.start_event_loop(0.05)
 
-        sleep(delay)
-
+        sleep(graph_delay)
